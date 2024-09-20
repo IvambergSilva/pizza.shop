1e2c00dfa9e733b49b4ac53dd08a9736277a3e74
IvambergSilva <ivambergisilva@gmail.com>
2024-09-20 00:05:11 -0300

    feat: add header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,7 +7,7 @@ import { AccountMenu } from "./account-menu";
 export function Header() {
     return (
         <header className="border-b">
-            <div className="flex h-16 items-center gap-6 px-6">
+            <div className="flex h-16 items-center gap-3 px-6 lg:px-16 lg:gap-6">
                 <Pizza className="h-6 w-6" />
                 <Separator
                     orientation="vertical"
