95d0ba59ca3ea64eaf3de5dd5124c70f641555e9
IvambergSilva <ivambergisilva@gmail.com>
2024-09-21 16:00:15 -0300

    feat: add app layout

--- src/_layouts/app.tsx
+++ src/_layouts/app.tsx
@@ -1,12 +1,12 @@
-import { Header } from "@/components/header"
-import { Outlet } from "react-router-dom"
+import { Header } from "@/components/header";
+import { Outlet } from "react-router-dom";
 
 export function AppLayout() {
     return (
         <div className="min-h-screen flex flex-col">
             <Header />
 
-            <div className="flex flex-1 flex-col gap-4 p-4 pt-6">
+            <div className="flex flex-1 flex-col gap-4 px-4 py-6 lg:px-16">
                 <Outlet />
             </div>
         </div>
