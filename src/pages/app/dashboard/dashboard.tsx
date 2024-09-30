bc9d88731a29ffeb7aa8c76847ecf5fac800d83a
IvambergSilva <ivambergisilva@gmail.com>
2024-09-29 23:04:37 -0300

    feat: add dashboard page with UI elements and skeleton loaders

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -2,9 +2,12 @@ import { Helmet } from "react-helmet-async"
 import { MonthOrdersAmountCard } from "./month-orders-amount-card"
 import { MonthRevenueCard } from "./month-revenue-card"
 import { DayOrdersAmountCard } from "./day-orders-amount-card"
+import { RevenueChart } from "./revenue-chart"
+import { PopularProductsChart } from "./popular-products-chart"
 import { MonthCanceledOrdersAmountCard } from "./month-canceled-orders-amount-card"
-import RevenueChart from "./revenue-chart"
-import PopularProductsChart from "./popular-products-chart"
+import { UpdateProduct } from "../../../components/update-product"
+import { Dialog, DialogTrigger } from "@/components/ui/dialog"
+import { GetCustomer } from "@/components/get-customer"
 
 export function Dashboard() {
     return (
@@ -16,10 +19,17 @@ export function Dashboard() {
                     <MonthRevenueCard />
                     <MonthOrdersAmountCard />
                     <DayOrdersAmountCard />
-                    <MonthCanceledOrdersAmountCard />
+                    {/* <MonthCanceledOrdersAmountCard /> */}
+                    <Dialog>
+                        <DialogTrigger>
+                            Abrir
+                        </DialogTrigger>
+                        <UpdateProduct />
+                    </Dialog>
+                    <GetCustomer />
                 </div>
 
-                <div className="grid grid-cols-1 md:grid-cols-9 gap-4">
+                <div className="grid grid-cols-1 mdLg:grid-cols-9 gap-4">
                     <RevenueChart />
                     <PopularProductsChart />
                 </div>
